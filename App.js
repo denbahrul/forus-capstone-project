f6f265e01ef36fbb0d53f3c4aa71700c78294463
maulanakhasan92 <90892270+maulanakhasan92@users.noreply.github.com>
2022-11-24 19:47:09 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hallo, Welcome in ForUs</h1>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
