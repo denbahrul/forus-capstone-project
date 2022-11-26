132eee53f60457ef48bcb67c5bc2a79f69f1cdaa
Muhammad Bahrul 'ulum <88572982+denbahrul@users.noreply.github.com>
2022-11-26 14:20:07 +0700

    Revert "Branch dimas"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-import "./styles/main.css";
+import './styles/main.css';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <App />,
 );
