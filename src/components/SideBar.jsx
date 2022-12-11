fa495afabed5ef27b38d7b649ea7059cf77a5e0d
denbahrul <ulum0326@gmail.com>
2022-12-11 22:16:03 +0700

    commit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import { AiOutlineFire, AiOutlineHome } from "react-icons/ai";
 import {FiLogOut} from 'react-icons/fi';
-import NewsItem from "./NewsItem";
 import {BiNews} from 'react-icons/bi'
 
 function RightBar() {
