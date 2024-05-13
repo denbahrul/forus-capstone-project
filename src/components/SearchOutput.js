f043240a3deae79fd89f977230962daf0f246be0
denbahrul <ulum0326@gmail.com>
2024-05-13 23:53:53 +0700

    Revert "resolve bug"
    This reverts commit a1720da4b40e088e7dc84149aa2067b515f720d3.

--- src/components/SearchOutput.js
+++ src/components/SearchOutput.js
@@ -2,7 +2,7 @@ import React from "react";
 import SearchNotFound from "./SearchNotFound";
 import { showFormattedDate } from "../utils";
 import { Link } from "react-router-dom";
-// import {AiOutlineUpCircle, AiOutlineDownCircle} from "react-icons/ai";
+import {AiOutlineUpCircle, AiOutlineDownCircle} from "react-icons/ai";
 import {FaRegComments} from "react-icons/fa";
 
 const SearchOutput = ({output}) => {
@@ -13,30 +13,30 @@ const SearchOutput = ({output}) => {
             (output[0] === 'empty') ? '' :
             output.map((item) => 
             <div className="post" key={item.id}>
-                <div className="post-item">
-                    <Link to={`/argument/${item.id}`}><h2>{item.title}</h2></Link>
-                    <p className="tanggal">{item.name} • <i>{showFormattedDate(item.createdAt)}</i></p>
-                    <div className="detail-kategori__instansi">
-                        <p className="detail-kategori">{item.kategori}</p>
-                        <p className="detail-instansi">{item.instansi}</p>
-                    </div>
-                    <p className="limit-char">{item.argument}</p>
-                    <div className="reaction-bar">
-                        {/* <div className="reaction-bar__vote">
-                            <AiOutlineUpCircle size={24} />
-                            <p>{item.upVote} Setuju</p>
-                        </div>
-                        <div className="reaction-bar__vote">
-                            <AiOutlineDownCircle size={24} />
-                            <p>{item.downVote} Tidak setuju</p>
-                        </div> */}
-                        <div className="reaction-bar__comments">
-                            <FaRegComments size={24} />
-                            <p>{item.comment.length} Komentar</p>
-                        </div>
-                    </div>
-                </div>
+            <div className="post-item">
+            <Link to={`/argument/${item.id}`}><h2>{item.title}</h2></Link>
+            <p className="tanggal">{item.name} • <i>{showFormattedDate(item.createdAt)}</i></p>
+            <div className="detail-kategori__instansi">
+                  <p className="detail-kategori">{item.kategori}</p>
+                  <p className="detail-instansi">{item.instansi}</p>
+              </div>
+            <p className="limit-char">{item.argument}</p>
+            <div className="reaction-bar">
+          <div className="reaction-bar__vote">
+              <AiOutlineUpCircle size={24} />
+              <p>{item.upVote} Setuju</p>
+          </div>
+          <div className="reaction-bar__vote">
+              <AiOutlineDownCircle size={24} />
+              <p>{item.downVote} Tidak setuju</p>
+          </div>
+          <div className="reaction-bar__comments">
+              <FaRegComments size={24} />
+              <p>{item.comment.length} Komentar</p>
+          </div>
+      </div>
             </div>
+          </div>
             ) : <SearchNotFound />
            }
         </div>
